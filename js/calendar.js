7e931e34fe0fbf8c50265d6e6f2c009f6d39f5ad
BoxingP <boxingp@gmail.com>
2015-02-04 19:45:49 +0800

    C-18: fix the mouse wheel bug in chrome and firefox. Boxing

--- js/calendar.js
+++ js/calendar.js
@@ -137,12 +137,18 @@
 
     Calendar.initialize();
 
-    $(".container").live("mousewheel DOMMouseScroll MozMousePixelScroll", function (event, delta) {
-        if (delta < 0) {
+    var mousewheelEvent = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel";
+
+    $(".container").live(mousewheelEvent, function (event) {
+        var evt = window.event || event;
+        var del = event.detail ? event.detail * (-120) : evt.wheelDelta;
+
+        if (del < 0) {
             $(this).find('.next').click();
         } else {
             $(this).find('.prev').click();
         }
+
         event.preventDefault();
         event.stopPropagation();
     });
