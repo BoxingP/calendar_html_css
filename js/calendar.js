01031425bb1c3cf4eaaf8b6977973d898ef81d22
BoxingP <boxingp@gmail.com>
2015-02-05 01:42:06 +0800

    C-20: put mouse wheel code to new file. Boxing

--- js/calendar.js
+++ js/calendar.js
@@ -142,20 +142,4 @@
 
     Calendar.initialize();
 
-    var mousewheelEvent = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel";
-
-    $(".container").live(mousewheelEvent, function (event) {
-        var evt = window.event || event;
-        var del = event.detail ? event.detail * (-120) : evt.wheelDelta;
-
-        if (del < 0) {
-            $(this).find('.next').click();
-        } else {
-            $(this).find('.prev').click();
-        }
-
-        event.preventDefault();
-        event.stopPropagation();
-    });
-    
 }(jQuery));
